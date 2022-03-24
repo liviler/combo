f9d086bcb3b076cf86c52637b6ddd4663f327433
liviler <liviler@163.com>
2022-03-24 21:22:18 +0800

    add help and texplay function etc to make  user better understand this package

--- __init__.py
+++ __init__.py
@@ -18,7 +18,82 @@ class bodys:
         leftIndices=[list(map(lambda x:symbols(x),i))for i in leftIndices]
         rightIndices=[list(map(lambda x:symbols(x),i))for i in rightIndices]
         self.incices=[leftIndices,rightIndices]
-    
+
+    def help(self):
+        '''
+        Display the meaning of attributes.
+        '''
+        print("# The attribute in class bodys")
+        print("Attribute".rjust(15),"Description")
+        atrMsg=[
+            ["gw","Generalized Wick Result"],
+            ["cmt", "gw1 - gw2"],
+            ["cmtXN","Apply xiRule and natRule to cmt"],
+            ["cmtXi","Apply xiRule to cmt"],
+            ["cmtNat","Apply natRule to cmt"],
+            ["allTerms","All Terms after applying  xiRule and natRule"],
+            ["filterTerms","Filter Result"],
+            ["canon","Canonicalized Result(Deal the deal term and rename the indices)"], 
+        ]
+        for i in atrMsg:
+            print((i[0]+':').rjust(15),i[1])
+
+        print("\n\n")
+        print("# The methods in class bodys")
+        print("Method".rjust(15),"Description")
+        mtdMsg=[
+            ["commutate","Caculate commutation relation."],
+            ["apply","Apply rule to the commutation relation result."],
+            ["regular","Filter your input body type and  Canonicalized the filter terms(deal the deal term and rename the indices )"],
+            ["amcExport","Form a amc document based on the regular result(after filtering)"],
+            ["amcEnd","Do the above 4 steps  in this function directly."],
+            ["texplay","Display all step result by latex in jupyter. "],
+
+        ]
+        for i in mtdMsg:
+            print((i[0]+':').rjust(15),i[1])
+
+
+        print("\n\n")
+        print("# The function in mboc")
+        print("Function".rjust(15),"Description")
+        fucMsg=[
+            ["texExp","Trans the expression to latex."],
+            ["uniteSame","Combine the same terms."],
+        ]
+        for i in fucMsg:
+            print((i[0]+':').rjust(15),i[1])
+
+    def texplay(self,jupyter="yes"):
+        '''
+        Display all step result by latex in jupyter.If you just want to show the latex expression, please set  the parameter: jupyter="no".
+        '''
+        attributes=["gw","cmt","cmtXN","cmtXi","cmtNat","allTerms","filterTerms","canon",]
+        if jupyter=="yes":
+            for i in attributes:
+                try:
+                    exp=getattr(self,i)
+                    laxExp=output.transSymbolsToLatex(exp)
+                    if i=="filterTerms":
+                       print( "filterTerms("+ str(filterBodyType) + "bodys) :")
+                    else:
+                        print(i,":") 
+                    output.jupyterTexDisplay(laxExp)
+                except:
+                    print('No attribute called:',i )
+        if jupyter=="no":
+            for i in attributes:
+                try:
+                    exp=getattr(self,i)
+                    laxExp=output.transSymbolsToLatex(exp)
+                    if i=="filterTerms":
+                       print( "filterTerms("+ str(filterBodyType) + "bodys) :")
+                    else:
+                        print(i,":") 
+                    print(laxExp) 
+                except:
+                    print('No attribute called:',i )
+
     def commutate(self):
         wickCase=Wick(self.incices[0],self.incices[1])
         wickCase.commmutate()
@@ -41,6 +116,8 @@ class bodys:
         self.allTerms=(G[tuple(self.incices[0][0]),tuple(self.incices[0][1])]*H[tuple(self.incices[1][0]),tuple(self.incices[1][1])]*self.cmtXN).expand()
         #Fliter
         if filterbody!=None and type(filterbody)==int:
+            global filterBodyType
+            filterBodyType=filterbody
             self.filterTerms=Filter.filterbody(self.allTerms,filterbody)
         else:
             self.filterTerms=self.allTerms
